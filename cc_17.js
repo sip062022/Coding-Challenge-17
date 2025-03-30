0839a5bf9f7c17db092e88958cc4ad610e428c33
Sahara Sipple <sahara36@usf.edu>
2025-03-29 23:45:13 -0400

    Task 2: Sales rep’s clients and total spent for a specific client.

--- cc_17.js
+++ cc_17.js
@@ -2,8 +2,8 @@
 
 class Customer { // adds customer class
     constructor (name, email) { // adds properties to constructor
-        this.name = name; // defines name
-        this.email = email; // defines email
+        this.name = name; // defines name property
+        this.email = email; // defines email property
         this.purchaseHistory = []; // defines array for purchase history
     }
 
@@ -16,3 +16,24 @@ class Customer { // adds customer class
     }
 }
 
+// Task 2: Create a Sales Rep Class //
+
+class salesRep { // creates salesRep class
+    constructor (name) { // adds name to the constructor
+        this.name = name;  // defines name property
+        this.clients = []; // creates an array for clients
+    }
+
+    addClient (customer) { // adds method addClient
+        this.clients.push(customer); // pushes the customers to the clients array
+    }
+
+    clientGetTotal (name) { // adds method clientGetTotal
+        const client = this.clients.find(client => client.name === name); // find exact match for client name
+        if (client) { // if the client is found
+            return client.getTotalSpent(); // pulls method from customer class to find total amount spent
+        } else {
+            return `Client not found.`; // error message
+        }
+    }
+}
